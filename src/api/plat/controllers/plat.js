87cb397f5f06873108028ed361a5e035130299f9
Emerson1220 <74653664+Emerson1220@users.noreply.github.com>
2022-08-01 18:12:28 +0200

    mise en place de l'API strapi

---
+++ src/api/plat/controllers/plat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  plat controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::plat.plat');
---
+++ src/api/plat/routes/plat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * plat router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::plat.plat');
